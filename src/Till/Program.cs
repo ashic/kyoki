43d7f5e64e9fcea71dfd73a0b9a4c583a7925c8d
ashic <ashic@live.com>
2014-05-28 11:41:06 +0100

    host for till

---
+++ src/Till/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Topshelf;
+
+namespace Till
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(x =>
+            {
+                x.Service<TillService>(
+                    s =>
+                    {
+                        s.ConstructUsing(() => new TillService());
+                        s.WhenStarted(_ => _.Start());
+                        s.WhenStopped(_ => _.Stop());
+                    });
+                
+                x.SetDescription("Till");
+                x.SetDisplayName("Till");
+                x.SetServiceName("Till");
+            });
+        }
+    }
+
+    public class TillService
+    {
+        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+        public TillService()
+        {
+            
+        }
+
+        public void Start()
+        {
+            
+        }
+
+        public void Stop()
+        {
+            
+        }
+    }
+}
